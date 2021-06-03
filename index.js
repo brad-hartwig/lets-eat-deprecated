34229565cce38d030aad65fb16b74efcfb22eef9
brad-hartwig <hartwig.brad@gmail.com>
2021-06-03 11:16:01 -0500

    add js to fetch json

---
+++ index.js
@@ -0,0 +1,9 @@
+var fetchJson = function(e){
+	$.getJSON( "https://bitbucket.org/crescendocollective/frontend-api-skills-test/src/master/data.json", function( data ) {
+		console.log(data);
+	});
+}
+
+$(document).ready(function() {
+  $('.warning').on('click', fetchJson);
+});
