5033e13656882cbacdfcc1e015f1476405e477cb
brad-hartwig <hartwig.brad@gmail.com>
2021-06-03 11:37:26 -0500

    localize json for test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fetchJson = function(e){
-	$.getJSON( "https://bitbucket.org/crescendocollective/frontend-api-skills-test/src/master/data.json", function( data ) {
+	// $.getJSON( "https://bitbucket.org/crescendocollective/frontend-api-skills-test/src/master/data.json", function( data ) {
+	$.getJSON( "data.json", function( data ) {
 		console.log(data);
 	});
 }
