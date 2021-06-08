1fbdf8407993e1fad050b8d6dce27bf3fc58d5b1
brad-hartwig <hartwig.brad@gmail.com>
2021-06-08 13:06:02 -0500

    interface with reviews shown; before recipe previews

--- index.js
+++
@@ -1,9 +0,0 @@
-const fetchJson = function(e){
-	$.getJSON( "http://localhost:3001/recipes", function( data ) {
-		console.log(data);
-	});
-}
-
-$(document).ready(function() {
-  $('.warning').on('click', fetchJson);
-});
