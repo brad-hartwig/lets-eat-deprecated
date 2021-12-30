8772581dd76d8176df1a0e9f4d3d03055b091499
brad-hartwig <hartwig.brad@gmail.com>
2021-12-30 11:40:28 -0600

    remove /rubanga in json path

--- public/js/index.js
+++ public/js/index.js
@@ -6,13 +6,13 @@
 
 const fetchJson = e => {
   // get recipes data
-  $.getJSON( "/rubanga/public/json/recipes.json", function(recipesData) {
+  $.getJSON( "/public/json/recipes.json", function(recipesData) {
   // $.getJSON( "http://localhost:3001/recipes", function(recipesData) {
     recipesStorage = recipesData;
     recipesStorage.map(createCards);
   });
   // get specials data
-  $.getJSON( "/rubanga/public/json/specials.json", function(specialsData) {
+  $.getJSON( "/public/json/specials.json", function(specialsData) {
   // $.getJSON( "http://localhost:3001/specials", function(specialsData) {
     specialsStorage = specialsData;
   });
